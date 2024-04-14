a509ec3978ce0e8a5301b321f140e64603d84716
wwwkieran <kieran@parikh.ca>
2024-04-14 21:55:44 +0000

    Deploying to gh-pages from @ wwwkieran/thirsty-gallerina-maps@7f15104f96d5ae4401500f51d176b88dbc275bc0 🚀

--- README.md
+++
@@ -1,1 +0,0 @@
-This is a website to show NYC gallery openings on a map. 
--- src/types/IOpening.ts
+++
@@ -1,8 +0,0 @@
-export default interface IOpening {
-    name: string,
-    date: number,
-    address: string,
-    description: string,
-    lat: number,
-    long: number,
-}
