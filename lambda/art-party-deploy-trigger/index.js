9d5cfe333ae7ee4ff73df68cb9e14307829ccda5
Kieran Parikh <kieran@parikh.ca>
2024-04-25 21:17:01 -0400

    deploy trigger

--- lambda/art-party-deploy-trigger/index.js
+++ lambda/art-party-deploy-trigger/index.js
@@ -1,4 +1,17 @@
+const {post} = require("axios");
 module.exports.run = async (event, context) => {
-  const time = new Date();
-  console.log(`Your cron function "${context.functionName}" ran at ${time}`);
+
+  post('https://api.github.com/repos/wwwkieran/thirsty-gallerina-maps/actions/workflows/main.yml/dispatches', {
+    ref: 'main',
+  }, {
+      headers: {
+          Authorization: "Bearer " + process.env.GITHUB_TOKEN
+      }
+  })
+      .then(function (response) {
+          console.log('Successfully triggered deploy')
+      })
+      .catch(function (error) {
+          console.log(error)
+      });
 };
