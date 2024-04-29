695f731613f21746cbe1953da4345262f64ba575
Kieran Parikh <kieran@parikh.ca>
2024-04-29 18:46:48 -0400

    lambda logic

--- lambda/art-party-deploy-trigger/index.js
+++ lambda/art-party-deploy-trigger/index.js
@@ -1,7 +1,36 @@
 const {post} = require("axios");
+const {igApi} = require("insta-fetcher");
+
+
 module.exports.run = async (event, context) => {
-    triggerDeploy()
+    if (await getLatestPostTimestamp() > await getLastDeployTimestamp()) {
+        console.log("Triggering deploy...")
+        triggerDeploy()
+    } else {
+        console.log("No deploy necessary!")
+    }
 };
+
+async function getLatestPostTimestamp() {
+    const ig = new igApi(process.env.INSTA_COOKIE)
+    const resp = await ig.fetchUserPostsV2('thirstygallerina')
+    return resp.edges[0].node.created_at
+}
+
+async function getLastDeployTimestamp() {
+    const resp = await post('https://api.github.com/repos/wwwkieran/artparty/deployments', {
+        ref: 'main',
+    }, {
+        headers: {
+            Authorization: "Bearer " + process.env.GITHUB_TOKEN
+        }
+    }).catch(function (error) {
+        console.log('Failed to get last deploy time!')
+        console.log(error)
+    });
+    console.log(resp)
+    return Date.now()
+}
 
 function triggerDeploy() {
     post('https://api.github.com/repos/wwwkieran/artparty/actions/workflows/main.yml/dispatches', {
@@ -12,9 +41,10 @@ function triggerDeploy() {
         }
     })
         .then(function (response) {
-            console.log('Successfully triggered deploy')
+            console.log('Successfully triggered deploy!')
         })
         .catch(function (error) {
+            console.log('Failed to trigger deploy!')
             console.log(error)
         });
 }
