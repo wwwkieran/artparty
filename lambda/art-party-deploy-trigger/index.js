02c6a77a04d026b48614e760c3494c68853b248f
Kieran Parikh <kieran@parikh.ca>
2024-04-29 17:14:58 -0400

    update deploy trigger link

--- lambda/art-party-deploy-trigger/index.js
+++ lambda/art-party-deploy-trigger/index.js
@@ -1,17 +1,20 @@
 const {post} = require("axios");
 module.exports.run = async (event, context) => {
-
-  post('https://api.github.com/repos/wwwkieran/thirsty-gallerina-maps/actions/workflows/main.yml/dispatches', {
-    ref: 'main',
-  }, {
-      headers: {
-          Authorization: "Bearer " + process.env.GITHUB_TOKEN
-      }
-  })
-      .then(function (response) {
-          console.log('Successfully triggered deploy')
-      })
-      .catch(function (error) {
-          console.log(error)
-      });
+    triggerDeploy()
 };
+
+function triggerDeploy() {
+    post('https://api.github.com/repos/wwwkieran/artparty/actions/workflows/main.yml/dispatches', {
+        ref: 'main',
+    }, {
+        headers: {
+            Authorization: "Bearer " + process.env.GITHUB_TOKEN
+        }
+    })
+        .then(function (response) {
+            console.log('Successfully triggered deploy')
+        })
+        .catch(function (error) {
+            console.log(error)
+        });
+}
